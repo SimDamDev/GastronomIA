d1de0607ffec7778b55b1e55510c831a7fa71bca
SimDamDev <contact@dev.simdam.ch>
2023-07-03 23:24:52 +0200

    Add routes for ingredients and recipes
    This commit adds routes for "/ingredients" and "/recipes" to handle GET requests. The routes respond with placeholder messages for now.

---
+++ src/routes/indexRoutes.js
@@ -0,0 +1,22 @@
+
+function routes(fastify, options, done) {
+    fastify.get('/ingredients', async (request, reply) => {
+      reply.send('Here will be all ingredients');
+    });
+  
+    fastify.get('/ingredients/add', async (request, reply) => {
+      reply.send('Here will be the form to add an ingredient');
+    });
+  
+    fastify.get('/recipes', async (request, reply) => {
+      reply.send('Here will be all recipes');
+    });
+  
+    fastify.get('/recipes/add', async (request, reply) => {
+      reply.send('Here will be the form to add a recipe');
+    });
+  
+    done();
+  }
+  
+  export default routes;

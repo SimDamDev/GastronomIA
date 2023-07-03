780bb96c0b4a2c3b83c34b61297fb7a62c5221e3
SimDamDev <contact@dev.simdam.ch>
2023-07-03 12:50:20 +0200

    refacto de app.js

--- src/app.js
+++ src/app.js
@@ -1,34 +1,41 @@
 import fastify from 'fastify';
-import "dotenv/config.js";
-import {MongoClient} from 'mongodb'
+import {dbConnect} from './services/db.js'
+import routes from './src/routes/indexRoutes.js';
+import errorHandler from './src/utils/errorHandler.js';
+import {config} from './config/config.js';
+//import {MongoClient} from 'mongodb'
 
 const app = fastify();
 
-console.log(process.env.MONGO_URI)
+dbConnect();
 
-app.get('/', async (request, reply) => {
-    const client = new MongoClient(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+app.register(routes)
+
+app.setErrorHandler(errorHandler)
+
+// app.get('/', async (request, reply) => {
+//     const client = new MongoClient(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
     
-    try {
-        await client.connect();
-        const database = client.db('gastronomIA');
-        const collection = database.collection('recipes');
-        const result = await collection.findOne({});
-        reply.send(result);
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-        reply.status(500).send('Internal Server Error');
-      } finally {
-        await client.close();
-      }
-    });
+//     try {
+//         await client.connect();
+//         const database = client.db('gastronomIA');
+//         const collection = database.collection('recipes');
+//         const result = await collection.findOne({});
+//         reply.send(result);
+//     } catch (error) {
+//         console.error('Error connecting to MongoDB:', error);
+//         reply.status(500).send('Internal Server Error');
+//       } finally {
+//         await client.close();
+//       }
+//     });
 
-const options = {
-    port: process.env.PORT,
-    host: process.env.HOST
-};
+// const options = {
+//     port: process.env.PORT,
+//     host: process.env.HOST
+// };
 
-app.listen(options, (err, address) => {
+app.listen(config.server, (err, address) => {
     if (err) {
         console.error(err);
         process.exit(1);

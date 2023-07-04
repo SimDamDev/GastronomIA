081b0a5dc8da5e440fdcf362f12bbb5a40f4a756
SimDamDev <contact@dev.simdam.ch>
2023-07-05 00:00:43 +0200

    - IMPROVE: Add validation to ensure name is unique - IMPROVE: Add validation to ensure nutritional values are positive numbers - IMPROVE: Add custom error message for invalid nutriScore - IMPROVE: Consider using null as default value or provide a default category - IMPROVE: Consider using null as default value or provide a default image - IMPROVE: Add validation to ensure image is a valid URL - IMPROVE: Add comments to explain what the seasonality field represents - IMPROVE: Add custom error message for invalid tasteNote - IMPROVE: Consider adding a virtual property to check if the ingredient is currently in season - FEATURE: Add a field for the origin of the ingredient - FEATURE: Add a field for possible substitutes of the ingredient - FEATURE: Add a field for potential allergens of the ingredient - FEATURE: Add a field for popular recipes using the ingredient - FEATURE: Add a field for the shelf life of the ingredient - FEATURE: Add a field for the storage method of the ingredient

--- src/models/ingredientModel.js
+++ src/models/ingredientModel.js
@@ -3,12 +3,12 @@ import mongoose from 'mongoose';
 import { tasteNoteEnum, nutriScoreEnum, monthEnum } from '../../config/constants.js';
 import { arrayLimit } from '../utils/validators.js';
 
-
 // Define the Ingredient schema
 const ingredientSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    // IMPROVE: Add validation to ensure name is unique
   },
   pluralName: {
     type: String,
@@ -23,27 +23,32 @@ const ingredientSchema = new mongoose.Schema({
     validate: [arrayLimit, '{PATH} needs at least 1 unit']
   },
   nutritionalValues: {
-    calories: Number, // Calories par 100g
-    proteins: Number, // Protéines par 100g
-    carbs: Number, // Glucides par 100g
-    fats: Number, // Lipides par 100g
-    fiber: Number, // Fibres par 100g
-    sugar: Number, // Sucres par 100g
-    sodium: Number, // Sodium par 100g
+    calories: Number, // Calories per 100g
+    proteins: Number, // Proteins per 100g
+    carbs: Number, // Carbs per 100g
+    fats: Number, // Fats per 100g
+    fiber: Number, // Fiber per 100g
+    sugar: Number, // Sugar per 100g
+    sodium: Number, // Sodium per 100g
+    // IMPROVE: Add validation to ensure nutritional values are positive numbers
   },
   nutriScore: {
     type: String,
     enum: nutriScoreEnum,
     required: false,
+    // IMPROVE: Add custom error message for invalid nutriScore
   },
   category: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Category-ingredient',
     default: ""/* Default category ID */,
+    // IMPROVE: Consider using null as default value or provide a default category
   }],
   image: {
     type: String,
     default: ""/* Default image URL */,
+    // IMPROVE: Consider using null as default value or provide a default image
+    // IMPROVE: Add validation to ensure image is a valid URL
   },
   seasonality: [{
     region: {
@@ -54,16 +59,19 @@ const ingredientSchema = new mongoose.Schema({
       type: String,
       enum: monthEnum,
     }],
+    // IMPROVE: Add comments to explain what this field represents
   }],
   tasteNote: {
     type: String,
     enum: tasteNoteEnum,
     required: false,
+    // IMPROVE: Add custom error message for invalid tasteNote
   },
   density: {
     type: Number,
     required: false, //need this for conversion between units in g/mL
   },
+  // IMPROVE: Consider adding a virtual property to check if the ingredient is currently in season
 });
 
 // Create the model
@@ -72,3 +80,9 @@ const Ingredient = mongoose.model('Ingredient', ingredientSchema);
 // Export the model
 export default Ingredient;
 
+//FEATURE: Add a field for the origin of the ingredient
+//FEATURE: Add a field for possible substitutes of the ingredient
+//FEATURE: Add a field for potential allergens of the ingredient
+//FEATURE: Add a field for popular recipes using the ingredient
+//FEATURE: Add a field for the shelf life of the ingredient
+//FEATURE: Add a field for the storage method of the ingredient

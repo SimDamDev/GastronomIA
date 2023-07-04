41a8db8b74a9c4c52cf12157fc0a9e3e469c6abb
SimDamDev <contact@dev.simdam.ch>
2023-07-04 22:53:33 +0200

    generation model ingredient v1

--- src/models/ingredientModel.js
+++ src/models/ingredientModel.js
@@ -1,5 +1,6 @@
 // Import necessary modules
 import mongoose from 'mongoose';
+import { tasteNoteEnum } from '../../config/constants';
 
 // Define the Ingredient schema
 const ingredientSchema = new mongoose.Schema({
@@ -7,11 +8,44 @@ const ingredientSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  quantity: {
-    type: Number,
-    required: true,
+  pluralName: {
+    type: String,
+    required: false,
   },
-  // Add more fields as necessary
+  units: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Unit-ingredient',
+  }],
+  nutritionalValues: {
+    calories: Number, // Calories par 100g
+    proteins: Number, // Protéines par 100g
+    carbs: Number, // Glucides par 100g
+    fats: Number, // Lipides par 100g
+    fiber: Number, // Fibres par 100g
+    sugar: Number, // Sucres par 100g
+    sodium: Number, // Sodium par 100g
+  },
+  nutriScore: {
+    type: String,
+    required: false,
+  },
+  category: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category-ingredient',
+  }],
+  image: {
+    type: String,
+    required: false,
+  },
+  seasonality: {
+    type: [String],
+    required: false,
+  },
+  tasteNote: {
+    type: String,
+    enum: tasteNoteEnum,
+    required: false,
+  },
 });
 
 // Create the model

8e9c35f9dab538ab8e97a9b1c7a3da814b0f7621
SimDamDev <contact@dev.simdam.ch>
2023-07-05 18:39:18 +0200

    ""

--- src/models/unitModel.js
+++ src/models/unitModel.js
@@ -6,23 +6,28 @@ const unitSchema = new mongoose.Schema({
     name: {
       type: String,
       required: true,
+      // IMPROVE: Add validation to ensure name is unique
     },
     abbreviation: {
       type: String,
       required: true,
+      // IMPROVE: Add validation to ensure abbreviation is unique
     },
     type: {
         type: String,
         enum: Object.values(unitType),
         required: false,
+        // IMPROVE: Add custom error message for invalid type
       },
       averageVolume: {
       type: Number,
       required: false,
+      // IMPROVE: Add validation to ensure averageVolume is a positive number
     },
     icon: {
       type: String,
       required: config.requireUnitIcons,
+      // IMPROVE: Add validation to ensure icon is a valid URL
     },
     isActive: {
       type: Boolean,
@@ -30,9 +35,13 @@ const unitSchema = new mongoose.Schema({
     },
   });
 
-
 // Create the model
 const Unit = mongoose.model('Unit', unitSchema);
 
 // Export the model
 export default Unit;
+
+//FEATURE: Add a field for the country or region where this unit is commonly used
+//FEATURE: Add a field for notes or additional information about the unit
+//FEATURE: Add a field for the date when the unit was added or last updated
+//FEATURE: Add a field for the user who added or last updated the unit

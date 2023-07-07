0f6381aaf74a2d5db3708de2929959ff87e2dc73
SimDamDev <contact@dev.simdam.ch>
2023-07-08 00:08:07 +0200

    Add static file serving for 'test-API' folder
    The 'staticFiles' function now includes additional code to serve static files from the 'test-API' folder. This is achieved by registering a new instance of 'fastifyStatic' with the appropriate root and prefix paths. The 'decorateReply' option is set to false for this new static file serving.

--- src/staticFiles.js
+++ src/staticFiles.js
@@ -8,5 +8,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 export default async function staticFiles(fastify, options) {
   fastify.register(fastifyStatic, {
     root: path.join(__dirname, '../public'),
+    prefix: '/', 
   });
+
+  // Servir les fichiers statiques de 'test-API'
+  fastify.register(fastifyStatic, {
+    root: path.join(__dirname, '../test-API'),
+    prefix: '/test-API/',
+    decorateReply: false
+  });  
 }

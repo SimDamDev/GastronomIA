e7bff1ba29a216e5c6753d5d2a799f119bd06da8
SimDamDev <contact@dev.simdam.ch>
2023-07-16 12:12:30 +0200

    aider: Refactored server.js to make the server port configurable and added error handling for server listening.
    # Aider chat conversation:
    
    USER: analyse mon fichier server.js et les fichier annexes, afin de pouvoir l'ameliorer
    
    ASSISTANT: Pour améliorer votre fichier `server.js`, je recommanderais d'ajouter une gestion d'erreur plus robuste et de rendre le port du serveur configurable. Cependant, je ne peux pas proposer des modifications pour les fichiers en lecture seule sans votre permission.
    
    Pour le fichier `server.js`, voici ce que je propose :
    
    ```javascript
    src\server.js
    <<<<<<< ORIGINAL
    import app from './app.js';
    import {config} from '../config/config.js';
    
    app.listen(config.server, (err, address) => {
        if (err) {
            console.error(`Error in ${__filename}: ${err}`);
            process.exit(1);
        }
    
        console.log(`Server listening at ${address}`);
    });
    =======
    import app from './app.js';
    import {config} from '../config/config.js';
    
    const port = process.env.PORT || config.server;
    
    app.listen(port, (err, address) => {
        if (err) {
            console.error(`Error in ${__filename}: ${err}`);
            process.exit(1);
        }
    
        console.log(`Server listening at ${address}`);
    }).on('error', (err) => {
        console.error(`Server error: ${err}`);
    });
    >>>>>>> UPDATED
    ```
    
    Ces modifications permettent de rendre le port du serveur configurable via une variable d'environnement, ce qui est une pratique courante pour déployer des applications dans différents environnements. De plus, j'ai ajouté une gestion d'erreur supplémentaire pour attraper les erreurs qui peuvent survenir lors de l'écoute du serveur.
    
    Pour les autres fichiers, je recommanderais de vérifier si vous suivez les meilleures pratiques en matière de structuration de code, de gestion d'erreur, de nommage des variables et de commentaires. Cependant, comme mentionné précédemment, je ne peux pas proposer des modifications pour ces fichiers sans votre permission.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import app from './app.js';
 import {config} from '../config/config.js';
 
-app.listen(config.server, (err, address) => {
+const port = process.env.PORT || config.server;
+
+app.listen(port, (err, address) => {
     if (err) {
         console.error(`Error in ${__filename}: ${err}`);
         process.exit(1);
     }
 
     console.log(`Server listening at ${address}`);
+}).on('error', (err) => {
+    console.error(`Server error: ${err}`);
 });
 

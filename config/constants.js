e40114c4d30c982a05f58b25afee8cd7b68bb7d4
SimDamDev <contact@dev.simdam.ch>
2023-07-16 12:22:13 +0200

    Refactor unit constants for better readability and maintainability.
    - Updated `unitType` to use an array for storing unit types.
    - Updated `baseUnit` to use an array for storing base unit values.
    - Removed unused `unitBase` object.

--- config/constants.js
+++ config/constants.js
@@ -4,8 +4,6 @@ export const nutriScoreEnum = ['A', 'B', 'C', 'D', 'E'];
 
 export const monthEnum = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-export const unitBase = {
-  'Solid': 'gramme',
-  'Liquid': 'millilitre',
-  'Universal': null
-};
+export const unitType = ['Solid','Liquid', 'Universal'];
+
+export const baseUnit = ['gramme', 'millilitre']

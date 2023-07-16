0af955b23162ba80ff01c1c182bbad1b82b36d64
SimDamDev <contact@dev.simdam.ch>
2023-07-16 12:05:37 +0200

    Updating config file with server settings
    The config file has been updated to include server settings such as port and host. The "requireUnitIcons" option has been set to false.

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ import "dotenv/config.js";
 export const config = {
 
     requireUnitIcons: false, // Change this to false if unit icons should not be required
-export const config = {
+    
     server:{
         port: process.env.PORT || 3000,
         host: process.env.HOST || 'localhost'

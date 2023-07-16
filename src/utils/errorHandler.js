c9b320f538e40808afb9870a027ba1cb4012da7d
SimDamDev <contact@dev.simdam.ch>
2023-07-16 12:30:03 +0200

    aider: Modify `errorHandler` to handle different types of errors by checking if the error has a message and sending it in the response, allowing it to handle a greater variety of errors.
    # Aider chat conversation:
    
    USER: tu peut faire les modif necessaires a `errorHandler` afin que il puisse etre utilisé ?
    
    ASSISTANT: Pour que `errorHandler` puisse être utilisé dans `server.js`, `app.js` et `staticFiles.js`, il doit être capable de gérer différents types d'erreurs. Actuellement, il ne gère que les erreurs de validation et les erreurs internes du serveur. Pour gérer d'autres types d'erreurs, vous pouvez ajouter des conditions supplémentaires à la fonction `errorHandler`.
    
    Voici comment vous pouvez modifier `errorHandler` pour gérer les erreurs de manière plus générique :
    
    ```javascript
    src\utils\errorHandler.js
    <<<<<<< ORIGINAL
    async function errorHandler (error, request, reply) {
        console.error(error);
    
        if (error.validation) {
          reply.status(400).send(error.message);
        } else {
          reply.status(500).send('Internal Server Error');
        }
      }
    
      export { errorHandler };
    =======
    async function errorHandler (error, request, reply) {
        console.error(error);
    
        if (error.validation) {
          reply.status(400).send(error.message);
        } else if (error.message) {
          reply.status(500).send(error.message);
        } else {
          reply.status(500).send('Internal Server Error');
        }
      }
    
      export { errorHandler };
    >>>>>>> UPDATED
    ```
    
    Dans cette modification, si l'erreur a un message, il sera envoyé dans la réponse. Sinon, un message d'erreur générique sera envoyé. Cela permet à `errorHandler` de gérer une plus grande variété d'erreurs.

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -3,6 +3,8 @@ async function errorHandler (error, request, reply) {
     
     if (error.validation) {
       reply.status(400).send(error.message);
+    } else if (error.message) {
+      reply.status(500).send(error.message);
     } else {
       reply.status(500).send('Internal Server Error');
     }

225845cf97edf15687be42ab843b2a67d0aabace
SimDamDev <contact@dev.simdam.ch>
2023-07-16 12:55:43 +0200

    aider: Added schema validations to all routes in `src/routes/regionRoutes.js`.
    # Aider chat conversation:
    
    USER: donc, si on effectue toute les modifications afin de rendre le fichier coeherent?
    
    ASSISTANT: Pour rendre le fichier cohérent, nous devons ajouter des validations de schéma pour toutes les routes dans `src\routes\regionRoutes.js`. Voici comment vous pourriez le faire :
    
    ```javascript
    src\routes\regionRoutes.js
    <<<<<<< ORIGINAL
        fastify.put('/regions/:id', RegionController.updateRegion);
    
        fastify.delete('/regions/:id', RegionController.removeRegion);
    
        fastify.get('/regions/count', RegionController.getTotalRegionCount);
    
        fastify.get('/regions/search', RegionController.searchRegions);
    
        fastify.get('/regions/:id/children', RegionController.getChildren);
    
        fastify.get('/regions/:id/parent', RegionController.getParent);
    
        fastify.patch('/regions/:id/move', RegionController.moveRegion);
    =======
        fastify.put('/regions/:id', {
            schema: {
                params: {
                    type: 'object',
                    required: ['id'],
                    properties: {
                        id: { type: 'string' },
                    },
                },
                body: {
                    type: 'object',
                    properties: {
                        name: { type: 'string' },
                        description: { type: 'string' },
                        parent: { type: 'string' },
                    },
                },
            },
        }, RegionController.updateRegion);
    
        fastify.delete('/regions/:id', {
            schema: {
                params: {
                    type: 'object',
                    required: ['id'],
                    properties: {
                        id: { type: 'string' },
                    },
                },
            },
        }, RegionController.removeRegion);
    
        fastify.get('/regions/count', {}, RegionController.getTotalRegionCount);
    
        fastify.get('/regions/search', {
            schema: {
                querystring: {
                    type: 'object',
                    properties: {
                        q: { type: 'string' },
                    },
                },
            },
        }, RegionController.searchRegions);
    
        fastify.get('/regions/:id/children', {
            schema: {
                params: {
                    type: 'object',
                    required: ['id'],
                    properties: {
                        id: { type: 'string' },
                    },
                },
            },
        }, RegionController.getChildren);
    
        fastify.get('/regions/:id/parent', {
            schema: {
                params: {
                    type: 'object',
                    required: ['id'],
                    properties: {
                        id: { type: 'string' },
                    },
                },
            },
        }, RegionController.getParent);
    
        fastify.patch('/regions/:id/move', {
            schema: {
                params: {
                    type: 'object',
                    required: ['id'],
                    properties: {
                        id: { type: 'string' },
                    },
                },
                body: {
                    type: 'object',
                    required: ['newParentId'],
                    properties: {
                        newParentId: { type: 'string' },
                    },
                },
            },
        }, RegionController.moveRegion);
    >>>>>>> UPDATED
    ```
    
    Dans ces modifications, nous avons ajouté des objets de schéma à toutes les routes. Ces objets de schéma définissent les champs requis et leurs types pour les paramètres de la requête, le corps de la requête et la chaîne de requête, selon ce qui est applicable pour chaque route. Si une requête ne respecte pas ces schémas, Fastify renverra automatiquement une réponse d'erreur 400 au client.
    
    Notez que ces modifications nécessitent que vous ayez le plugin 'fastify-swagger' installé et configuré dans votre application. Si ce n'est pas le cas, vous devrez l'installer et le configurer avant de pouvoir utiliser cette fonctionnalité.

--- src/routes/regionRoutes.js
+++ src/routes/regionRoutes.js
@@ -30,19 +30,93 @@ function routes(fastify, options, done) {
 
     fastify.get('/regions', RegionController.getAllRegions);
 
-    fastify.put('/regions/:id', RegionController.updateRegion);
+    fastify.put('/regions/:id', {
+        schema: {
+            params: {
+                type: 'object',
+                required: ['id'],
+                properties: {
+                    id: { type: 'string' },
+                },
+            },
+            body: {
+                type: 'object',
+                properties: {
+                    name: { type: 'string' },
+                    description: { type: 'string' },
+                    parent: { type: 'string' },
+                },
+            },
+        },
+    }, RegionController.updateRegion);
 
-    fastify.delete('/regions/:id', RegionController.removeRegion);
+    fastify.delete('/regions/:id', {
+        schema: {
+            params: {
+                type: 'object',
+                required: ['id'],
+                properties: {
+                    id: { type: 'string' },
+                },
+            },
+        },
+    }, RegionController.removeRegion);
 
-    fastify.get('/regions/count', RegionController.getTotalRegionCount);
+    fastify.get('/regions/count', {}, RegionController.getTotalRegionCount);
 
-    fastify.get('/regions/search', RegionController.searchRegions);
+    fastify.get('/regions/search', {
+        schema: {
+            querystring: {
+                type: 'object',
+                properties: {
+                    q: { type: 'string' },
+                },
+            },
+        },
+    }, RegionController.searchRegions);
 
-    fastify.get('/regions/:id/children', RegionController.getChildren);
+    fastify.get('/regions/:id/children', {
+        schema: {
+            params: {
+                type: 'object',
+                required: ['id'],
+                properties: {
+                    id: { type: 'string' },
+                },
+            },
+        },
+    }, RegionController.getChildren);
 
-    fastify.get('/regions/:id/parent', RegionController.getParent);
+    fastify.get('/regions/:id/parent', {
+        schema: {
+            params: {
+                type: 'object',
+                required: ['id'],
+                properties: {
+                    id: { type: 'string' },
+                },
+            },
+        },
+    }, RegionController.getParent);
 
-    fastify.patch('/regions/:id/move', RegionController.moveRegion);
+    fastify.patch('/regions/:id/move', {
+        schema: {
+            params: {
+                type: 'object',
+                required: ['id'],
+                properties: {
+                    id: { type: 'string' },
+                },
+            },
+            body: {
+                type: 'object',
+                required: ['newParentId'],
+                properties: {
+                    newParentId: { type: 'string' },
+                },
+            },
+        },
+    }, RegionController.moveRegion);
     
     done();
 }

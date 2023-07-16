2d9a9f86dd08dc513a6ccff4ce2949c79b3728bb
SimDamDev <contact@dev.simdam.ch>
2023-07-16 13:26:34 +0200

    aider: Added validation schemas for the routes POST `/units`, PUT `/units/:id`, and PATCH `/units/:id/status` in `unitRoutes.js` to make it consistent with `regionRoutes.js`.
    # Aider chat conversation:
    
    USER: en prenant compte regionRoutes, rend coherent l'ensemble des fichier
    
    ASSISTANT: Pour rendre `unitRoutes.js` cohérent avec `regionRoutes.js`, nous pouvons ajouter des schémas de validation pour les routes. Voici comment nous pourrions le faire :
    
    1. Ajouter un schéma de validation pour la route POST `/units`. Ce schéma vérifiera que le corps de la requête contient les champs requis pour créer une unité.
    2. Ajouter un schéma de validation pour la route PUT `/units/:id`. Ce schéma vérifiera que le corps de la requête contient les champs requis pour mettre à jour une unité.
    3. Ajouter un schéma de validation pour la route PATCH `/units/:id/status`. Ce schéma vérifiera que le corps de la requête contient le champ `status`.
    
    Voici les blocs de modification pour chaque route :
    
    ```javascript
    src\routes\unitRoutes.js
    <<<<<<< ORIGINAL
        fastify.post('/units', UnitController.createUnit);
    =======
        fastify.post('/units', {
            schema: {
                body: {
                    type: 'object',
                    required: ['name', 'abbreviation', 'baseUnit'],
                    properties: {
                        name: { type: 'string' },
                        abbreviation: { type: 'string' },
                        type: { type: 'string' },
                        conversionFactor: { type: 'number' },
                        baseUnit: { type: 'string' },
                        icon: { type: 'string' },
                        isActive: { type: 'boolean' },
                    },
                },
            },
        }, UnitController.createUnit);
    >>>>>>> UPDATED
    ```
    
    ```javascript
    src\routes\unitRoutes.js
    <<<<<<< ORIGINAL
        fastify.put('/units/:id', UnitController.updateUnit);
    =======
        fastify.put('/units/:id', {
            schema: {
                params: {
                    type: 'object',
                    required: ['id'],
                    properties: {
                        id: { type: 'string' },
                    },
                },
                body: {
                    type: 'object',
                    properties: {
                        name: { type: 'string' },
                        abbreviation: { type: 'string' },
                        type: { type: 'string' },
                        conversionFactor: { type: 'number' },
                        baseUnit: { type: 'string' },
                        icon: { type: 'string' },
                        isActive: { type: 'boolean' },
                    },
                },
            },
        }, UnitController.updateUnit);
    >>>>>>> UPDATED
    ```
    
    ```javascript
    src\routes\unitRoutes.js
    <<<<<<< ORIGINAL
        fastify.patch('/units/:id/status', UnitController.updateUnitStatus);
    =======
        fastify.patch('/units/:id/status', {
            schema: {
                params: {
                    type: 'object',
                    required: ['id'],
                    properties: {
                        id: { type: 'string' },
                    },
                },
                body: {
                    type: 'object',
                    required: ['status'],
                    properties: {
                        status: { type: 'boolean' },
                    },
                },
            },
        }, UnitController.updateUnitStatus);
    >>>>>>> UPDATED
    ```
    
    Ces modifications permettront de valider les données d'entrée pour les routes POST, PUT et PATCH, et de renvoyer une réponse appropriée au client si les données ne sont pas valides.

--- src/routes/unitRoutes.js
+++ src/routes/unitRoutes.js
@@ -2,13 +2,51 @@ import UnitController from '../controllers/unitController.js';
 
 function routes(fastify, options, done) {
 
-    fastify.post('/units', UnitController.createUnit);
+    fastify.post('/units', {
+        schema: {
+            body: {
+                type: 'object',
+                required: ['name', 'abbreviation', 'baseUnit'],
+                properties: {
+                    name: { type: 'string' },
+                    abbreviation: { type: 'string' },
+                    type: { type: 'string' },
+                    conversionFactor: { type: 'number' },
+                    baseUnit: { type: 'string' },
+                    icon: { type: 'string' },
+                    isActive: { type: 'boolean' },
+                },
+            },
+        },
+    }, UnitController.createUnit);
 
     fastify.get('/units/:id', UnitController.getUnit);
 
     fastify.get('/units', UnitController.getAllUnits);
 
-    fastify.put('/units/:id', UnitController.updateUnit);
+    fastify.put('/units/:id', {
+        schema: {
+            params: {
+                type: 'object',
+                required: ['id'],
+                properties: {
+                    id: { type: 'string' },
+                },
+            },
+            body: {
+                type: 'object',
+                properties: {
+                    name: { type: 'string' },
+                    abbreviation: { type: 'string' },
+                    type: { type: 'string' },
+                    conversionFactor: { type: 'number' },
+                    baseUnit: { type: 'string' },
+                    icon: { type: 'string' },
+                    isActive: { type: 'boolean' },
+                },
+            },
+        },
+    }, UnitController.updateUnit);
 
     fastify.delete('/units/:id', UnitController.removeUnit);
 
@@ -16,7 +54,24 @@ function routes(fastify, options, done) {
 
     fastify.get('/units/search', UnitController.searchUnits);
 
-    fastify.patch('/units/:id/status', UnitController.updateUnitStatus);
+    fastify.patch('/units/:id/status', {
+        schema: {
+            params: {
+                type: 'object',
+                required: ['id'],
+                properties: {
+                    id: { type: 'string' },
+                },
+            },
+            body: {
+                type: 'object',
+                required: ['status'],
+                properties: {
+                    status: { type: 'boolean' },
+                },
+            },
+        },
+    }, UnitController.updateUnitStatus);
 
     fastify.get('/units/status/:status', UnitController.getUnitsByStatus);
 

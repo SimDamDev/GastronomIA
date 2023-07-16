ce0e12363575b06df1b749b660a3ec554064ced0
SimDamDev <contact@dev.simdam.ch>
2023-07-16 12:37:45 +0200

    Refactor validateRegionId function to use a more descriptive parameter name.y
    # Aider chat conversation:

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -2,9 +2,9 @@ export function isNotEmptyArray(val) {
     return val.length > 0;
   }import Region from '../models/regionModel.js';
 
-async function validateRegionId(v) {
-  if (v === null) return true;
-  return await Region.findById(v) !== null;
+async function validateRegionId(val) {
+  if (val === null) return true;
+  return await Region.findById(val) !== null;
 }
 
 export { validateRegionId };

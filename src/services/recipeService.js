14b37cf55f47af2cc6305fbe4dcb0aeeb70548b2
SimDamDev <contact@dev.simdam.ch>
2023-07-17 19:46:39 +0200

    aider: Refactored recipeService.js to add JSDoc comments for better code documentation.
    # Aider chat conversation:
    
    USER: i agree
    
    ASSISTANT: Great! Let's proceed with the refactoring of the remaining service files.
    
    We'll start with `src\services\recipeService.js`. We'll add JSDoc comments to improve the code documentation and make the code more understandable and maintainable.
    
    Here is how you can refactor the `src\services\recipeService.js` file:
    
    ```javascript
    src\services\recipeService.js
    <<<<<<< ORIGINAL
    // Import necessary models
    import { Recipe } from '../models/indexModel.js';
    
    export async function getRecipesService() {
      // Here you can get all recipes from your database
      // return await Recipe.find();
    }
    
    export async function addRecipeService(recipeData) {
      // Here you can create a new recipe in your database
      // return await Recipe.create(recipeData);
    }
    =======
    /**
     * This file contains the services for recipes.
     */
    
    // Import necessary models
    import { Recipe } from '../models/indexModel.js';
    
    /**
     * Get all recipes.
     * @returns {Promise<Array>} The recipes.
     */
    export async function getRecipesService() {
      // Here you can get all recipes from your database
      // return await Recipe.find();
    }
    
    /**
     * Add a new recipe.
     * @param {Object} recipeData - The data to create the recipe with.
     * @returns {Promise<Object>} The created recipe.
     */
    export async function addRecipeService(recipeData) {
      // Here you can create a new recipe in your database
      // return await Recipe.create(recipeData);
    }
    >>>>>>> UPDATED
    ```
    
    Please apply these changes and let me know if you have any questions or if there's anything else you'd like to refactor.

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -1,11 +1,24 @@
+/**
+ * This file contains the services for recipes.
+ */
+
 // Import necessary models
 import { Recipe } from '../models/indexModel.js';
 
+/**
+ * Get all recipes.
+ * @returns {Promise<Array>} The recipes.
+ */
 export async function getRecipesService() {
   // Here you can get all recipes from your database
   // return await Recipe.find();
 }
 
+/**
+ * Add a new recipe.
+ * @param {Object} recipeData - The data to create the recipe with.
+ * @returns {Promise<Object>} The created recipe.
+ */
 export async function addRecipeService(recipeData) {
   // Here you can create a new recipe in your database
   // return await Recipe.create(recipeData);

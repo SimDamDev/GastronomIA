2dba921e9dfac8cb106f6acf21a531e660f5e5b0
SimDamDev <contact@dev.simdam.ch>
2023-07-17 19:53:05 +0200

    aider: Refactored the `src\services\indexService.js` file to add JSDoc comments for improved code documentation.
    # Aider chat conversation:
    
    USER: continue
    
    ASSISTANT: Let's continue with the refactoring of the `src\services\indexService.js` file. We'll add JSDoc comments to improve the code documentation and make the code more understandable and maintainable.
    
    Here is how you can refactor the `src\services\indexService.js` file:
    
    ```javascript
    src\services\indexService.js
    <<<<<<< ORIGINAL
    // Import services
    import {getIngredientsService, addIngredientService} from './ingredientService.js';
    import {getRecipesService, addRecipeService} from './recipeService.js';
    
    // Export services
    export {getIngredientsService, addIngredientService, getRecipesService, addRecipeService};
    =======
    /**
     * This file imports and exports all services.
     */
    
    // Import services
    import {getIngredientsService, addIngredientService} from './ingredientService.js';
    import {getRecipesService, addRecipeService} from './recipeService.js';
    
    /**
     * Export all services.
     */
    export {getIngredientsService, addIngredientService, getRecipesService, addRecipeService};
    >>>>>>> UPDATED
    ```
    
    Please apply these changes and let me know if you have any questions or if there's anything else you'd like to refactor.

--- src/services/indexService.js
+++ src/services/indexService.js
@@ -1,6 +1,12 @@
+/**
+ * This file imports and exports all services.
+ */
+
 // Import services
 import {getIngredientsService, addIngredientService} from './ingredientService.js';
 import {getRecipesService, addRecipeService} from './recipeService.js';
 
-// Export services
+/**
+ * Export all services.
+ */
 export {getIngredientsService, addIngredientService, getRecipesService, addRecipeService};

b62097ec55d4971131ce03caf65ad6bf97384eba
SimDamDev <contact@dev.simdam.ch>
2023-07-17 19:04:59 +0200

    aider: Centralized the import of all models in indexModel.js and exported all models.
    # Aider chat conversation:
    
    USER: est ce que je devrais centtralise le import de tous mes models dans indexModel.js, et ensuite importer ce dernier dans mes services?
    
    ASSISTANT: Oui, centraliser l'importation de tous vos modèles dans un seul fichier (comme indexModel.js) peut rendre votre code plus propre et plus facile à gérer. Ensuite, vous pouvez simplement importer ce fichier dans vos services.
    
    Voici les étapes pour le faire:
    
    1. Modifiez le fichier `src\models\indexModel.js` pour inclure tous les modèles.
    
    2. Modifiez les fichiers de service pour importer les modèles à partir de `indexModel.js` au lieu de les importer directement.
    
    Voici comment vous pouvez le faire:
    
    ```javascript
    src\models\indexModel.js
    <<<<<<< ORIGINAL
    // This file imports and exports the Ingredient and Recipe models.
    
    // Import models
    import Ingredient from './ingredientModel.js';
    import Recipe from './recipeModel.js';
    
    // Export models
    export {Ingredient, Recipe};
    =======
    // This file imports and exports all models.
    
    // Import models
    import Ingredient from './ingredientModel.js';
    import Recipe from './recipeModel.js';
    import Unit from './unitModel.js';
    import Region from './regionModel.js';
    import IngredientCategory from './ingredientCategory.js';
    
    // Export models
    export {Ingredient, Recipe, Unit, Region, IngredientCategory};
    >>>>>>> UPDATED
    ```
    
    Ensuite, dans vos fichiers de service, vous pouvez importer les modèles comme ceci:
    
    ```javascript
    src\services\exampleService.js
    <<<<<<< ORIGINAL
    import Ingredient from '../models/ingredientModel.js';
    import Recipe from '../models/recipeModel.js';
    =======
    import { Ingredient, Recipe } from '../models/indexModel.js';
    >>>>>>> UPDATED
    ```
    
    Remplacez `exampleService.js` par le nom de vos fichiers de service. Répétez cette étape pour chaque fichier de service qui importe des modèles.

--- src/models/indexModel.js
+++ src/models/indexModel.js
@@ -1,8 +1,11 @@
-// This file imports and exports the Ingredient and Recipe models.
+// This file imports and exports all models.
 
 // Import models
 import Ingredient from './ingredientModel.js';
 import Recipe from './recipeModel.js';
+import Unit from './unitModel.js';
+import Region from './regionModel.js';
+import IngredientCategory from './ingredientCategory.js';
 
 // Export models
-export {Ingredient, Recipe};
+export {Ingredient, Recipe, Unit, Region, IngredientCategory};

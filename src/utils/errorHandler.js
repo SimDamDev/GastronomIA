75dc90022843ce296a957a403e2efe1eb1f51a40
SimDamDev <contact@dev.simdam.ch>
2023-07-17 19:41:17 +0200

    aider: Refactored errorHandler.js to handle duplication errors and updated services to use errorHandler instead of handleError.
    # Aider chat conversation:
    
    USER: I added these *read-write* files: src\utils\errorHandler.js
    
    ASSISTANT: Great! Now that we have access to the `errorHandler.js` file, we can modify it as needed. What changes would you like to make?
    
    USER: je vois que tu as ajoute helpers.js cependant dans errorHandler , j'ai l'impression que on faisait deja un truc similaire
    
    ASSISTANT: Vous avez raison, `errorHandler.js` semble également gérer les erreurs. Cependant, la fonction `handleError` que nous avons ajoutée dans `helpers.js` est spécifiquement pour gérer les erreurs de duplication de la base de données (erreur 11000), qui sont courantes dans les méthodes `create` et `update` de vos services.
    
    Si vous préférez, nous pouvons déplacer cette fonctionnalité dans `errorHandler.js`. Cela pourrait ressembler à ceci :
    
    ```javascript
    src\utils\errorHandler.js
    <<<<<<< ORIGINAL
    async function errorHandler(error, request, reply) {
      console.error(error);
    
      if (error.validation) {
        reply.status(400).send(error.validation);
      } else if (error.message) {
        reply.status(500).send(error.message);
      } else {
        reply.status(500).send('Internal Server Error');
      }
    }
    =======
    async function errorHandler(error, request, reply) {
      console.error(error);
    
      if (error.code === 11000) {
        reply.status(400).send('Name must be unique');
      } else if (error.validation) {
        reply.status(400).send(error.validation);
      } else if (error.message) {
        reply.status(500).send(error.message);
      } else {
        reply.status(500).send('Internal Server Error');
      }
    }
    >>>>>>> UPDATED
    ```
    
    Et puis, nous devons mettre à jour les services pour utiliser `errorHandler` au lieu de `handleError` :
    
    ```javascript
    src\services\unitService.js
    <<<<<<< ORIGINAL
    import { handleError } from '../utils/helpers.js';
    
        try {
          const unit = new Unit(data);
          await unit.save();
          return unit;
        } catch (error) {
          handleError(error);
        }
    =======
    import { errorHandler } from '../utils/errorHandler.js';
    
        try {
          const unit = new Unit(data);
          await unit.save();
          return unit;
        } catch (error) {
          errorHandler(error);
        }
    >>>>>>> UPDATED
    ```
    
    Veuillez noter que cette modification nécessite que `errorHandler` soit modifié pour fonctionner sans les paramètres `request` et `reply`, ou que ces paramètres soient passés lors de l'appel à `errorHandler` dans les services.

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,7 +1,9 @@
 async function errorHandler(error, request, reply) {
   console.error(error);
 
-  if (error.validation) {
+  if (error.code === 11000) {
+    reply.status(400).send('Name must be unique');
+  } else if (error.validation) {
     reply.status(400).send(error.validation);
   } else if (error.message) {
     reply.status(500).send(error.message);

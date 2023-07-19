cd2fdc3c2a0efbcac6742750062311f61dbde863
SimDamDev <contact@dev.simdam.ch>
2023-07-19 12:55:48 +0200

    aider: Added comments to clarify the purpose of each exported function in `index.js`.
    # Aider chat conversation:
    
    USER: dans index.js c'est utile de rajouter des commentaire?
    
    ASSISTANT: Oui, il peut être utile d'ajouter des commentaires dans `index.js` pour expliquer ce que fait chaque exportation. Cela peut aider d'autres développeurs à comprendre rapidement le but de chaque fonction exportée. Voici comment nous pourrions le faire :
    
    ```javascript
    src\utils\validators\index.js
    <<<<<<< ORIGINAL
    import configValidator from './configValidator';
    import { isPositiveNumber } from './arrayValidator';
    import { validateRegionId } from './regionValidator';
    import { isNotEmptyArray } from './arrayValidator';
    
    export default {configValidator, isPositiveNumber, validateRegionId, isNotEmptyArray}
    =======
    import configValidator from './configValidator';
    import { isPositiveNumber } from './arrayValidator';
    import { validateRegionId } from './regionValidator';
    import { isNotEmptyArray } from './arrayValidator';
    
    /**
     * Exports the configValidator, isPositiveNumber, validateRegionId, and isNotEmptyArray functions.
     *
     * @module
     * @name Validators
     * @property {function} configValidator - Validates the provided configuration object.
     * @property {function} isPositiveNumber - Checks if a given number is positive.
     * @property {function} validateRegionId - Validates the provided region ID.
     * @property {function} isNotEmptyArray - Checks if a given array is not empty.
     */
    export default {configValidator, isPositiveNumber, validateRegionId, isNotEmptyArray}
    >>>>>>> UPDATED
    ```
    
    Cela devrait aider à clarifier l'objectif de chaque fonction pour quiconque lit le code.

--- src/utils/validators/index.js
+++ src/utils/validators/index.js
@@ -3,5 +3,14 @@ import { isPositiveNumber } from './arrayValidator';
 import { validateRegionId } from './regionValidator';
 import { isNotEmptyArray } from './arrayValidator';
 
-
+/**
+ * Exports the configValidator, isPositiveNumber, validateRegionId, and isNotEmptyArray functions.
+ *
+ * @module
+ * @name Validators
+ * @property {function} configValidator - Validates the provided configuration object.
+ * @property {function} isPositiveNumber - Checks if a given number is positive.
+ * @property {function} validateRegionId - Validates the provided region ID.
+ * @property {function} isNotEmptyArray - Checks if a given array is not empty.
+ */
 export default {configValidator, isPositiveNumber, validateRegionId, isNotEmptyArray}

b186fde3c8bc95ec375f71f258b7484e770d3ec4
SimDamDev <contact@dev.simdam.ch>
2023-07-19 12:55:02 +0200

    aider: Added a comment to the `isPositiveNumber` function in `numberValidator.js` to explain its purpose.
    # Aider chat conversation:
    
    USER: e tnumber validator, ainsi que array validator, c'est des fils uniques?
    
    ASSISTANT: Oui, d'après les fichiers que vous avez fournis, `numberValidator.js` et `arrayValidator.js` sont des fichiers uniques. Ils ne semblent pas être importés par d'autres fichiers que `index.js` dans le dossier `validators`. Cela signifie que les fonctions qu'ils exportent (`isPositiveNumber` pour `numberValidator.js` et `isNotEmptyArray` pour `arrayValidator.js`) sont uniquement utilisées à travers l'objet exporté par `index.js`.
    
    USER: oui daccord, mais les commentaires??
    
    ASSISTANT: Je vois que vous avez déjà ajouté des commentaires pour la fonction `isNotEmptyArray` dans `arrayValidator.js`. Pour la fonction `isPositiveNumber` dans `numberValidator.js`, nous pouvons ajouter un commentaire similaire pour expliquer ce que fait la fonction. Voici comment nous pourrions le faire :
    
    ```javascript
    src\utils\validators\numberValidator.js
    <<<<<<< ORIGINAL
    export async function isPositiveNumber(val) {
        return val > 0;
    }
    =======
    /**
     * Checks if a given number is positive.
     *
     * @function
     * @name isPositiveNumber
     * @param {number} val - The number to check.
     * @returns {boolean} Returns `true` if the number is positive, `false` otherwise.
     */
    export async function isPositiveNumber(val) {
        return val > 0;
    }
    >>>>>>> UPDATED
    ```
    
    Cela devrait aider à clarifier l'objectif de la fonction pour quiconque lit le code.

--- src/utils/validators/numberValidator.js
+++ src/utils/validators/numberValidator.js
@@ -1,3 +1,11 @@
+/**
+ * Checks if a given number is positive.
+ *
+ * @function
+ * @name isPositiveNumber
+ * @param {number} val - The number to check.
+ * @returns {boolean} Returns `true` if the number is positive, `false` otherwise.
+ */
 export async function isPositiveNumber(val) {
     return val > 0;
-  }
+}

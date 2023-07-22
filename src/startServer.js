62801ccecf28965ea6a96e71f8f4324ca3d32bb3
SimDamDev <contact@dev.simdam.ch>
2023-07-23 00:26:46 +0200

    Add .babelrc and jest.config.js files, and modify the startServer function in src/startServer.js. Also, add unit tests for the startServer function in test/startServer.test.js.
    - Add .babelrc file with preset-env configuration.
    - Add jest.config.js file with transform and testEnvironment configurations.
    - Modify the startServer function in src/startServer.js to include host parameter, handle system signals, log error messages, and assign the server to app.server.
    - Update the unit tests in test/startServer.test.js to use sinon and chai libraries, and include tests for server startup, error handling, and signal handling.

--- src/startServer.js
+++ src/startServer.js
@@ -1,31 +1,68 @@
-import app from './app.js';
-import {config} from './config/config.js';
-import {errorHandler} from './middlewares/errorHandler.js';
+import { getConfig } from '../config/config.js';
 
 /**
  * Starts the server.
- * Retrieves the port from the configuration and listens for incoming connections.
+ * Retrieves the port and host from the configuration and listens for incoming connections.
+ * @param {import('fastify').FastifyInstance} app The Fastify application to start.
  * @function
  * @name startServer
  * @returns {Promise} Resolves when the server has started successfully.
  */
-export function startServer() {
-  const  {port, host} = config.server;
+export function startServer(app, server) { 
+  
+  // Add server parameter
+  if (app.server) {
+    throw new Error('Server is already running');
+  }
+
+  app.server = server;  // Assign server to app.server here
+
+  const config = getConfig();
+  const { port, host } = config.server;
+
+  // Handle system signals
+  process.on('SIGINT', () => {
+    if (app.server) {
+      app.server.close((err) => {  // Add err parameter
+        if (err) {
+          app.log.error('Server close failed');  // Log error message
+        } else {
+          app.log.info('Server closed due to SIGINT signal');
+        }
+      });
+    }
+  });
+
+  let isClosed = false;
+
+  process.on('SIGTERM', () => {
+    if (!app.server) return;
+  
+    app.log.info('Received SIGTERM signal, shutting down server...');
+  
+    app.server.close(() => {
+      if (isClosed) return;
+  
+      isClosed = true;
+      app.log.info('Server closed successfully');  
+      process.exit(0);
+    });
+  });
 
   return new Promise((resolve, reject) => {
-    const server = app.listen(port, host, (err) => {
-      if (err){
-        errorHandler(err);
+    app.listen(port, host, (err, server) => {
+      if (err) {
+        if (err.code === 'EADDRINUSE') {
+          app.log.error('Port already in use'); 
+        } else {
+          app.log.error(err);
+        }
         reject(err);
       } else {
         app.log.info(`Server listening on ${host}:${port}`);
-        resolve(server);
+        resolve();
       }
     });
-
-    // Listen for SIGINT and SIGTERM signals and close the server gracefully
-    process.on('SIGINT', () => server.close());
-    process.on('SIGTERM', () => server.close());
   });
 }
 

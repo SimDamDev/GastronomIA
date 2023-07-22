ce68760bf993e1491db514eaaccfa81cae3f9898
SimDamDev <contact@dev.simdam.ch>
2023-07-23 00:32:26 +0200

    feat: Add signal handlers to gracefully shutdown the server
    This commit adds signal handlers to gracefully handle SIGINT and SIGTERM signals. The server is closed and the status is logged when a signal is received.

--- src/startServer.js
+++ src/startServer.js
@@ -1,6 +1,44 @@
 import { getConfig } from '../config/config.js';
 
 /**
+ * Handle SIGINT signal.
+ * Closes the server and logs the status.
+ * @param {import('fastify').FastifyInstance} app The Fastify application.
+ */
+function handleSIGINT(app) {
+  if (app.server) {
+    app.server.close((err) => {
+      if (err) {
+        app.log.error('Server close failed');
+      } else {
+        app.log.info('Server closed due to SIGINT signal');
+      }
+    });
+  }
+}
+
+/**
+ * Handle SIGTERM signal.
+ * Closes the server and logs the status.
+ * @param {import('fastify').FastifyInstance} app The Fastify application.
+ */
+function handleSIGTERM(app) {
+  let isClosed = false;
+
+  if (!app.server) return;
+
+  app.log.info('Received SIGTERM signal, shutting down server...');
+
+  app.server.close(() => {
+    if (isClosed) return;
+
+    isClosed = true;
+    app.log.info('Server closed successfully');  
+    process.exit(0);
+  });
+}
+
+/**
  * Starts the server.
  * Retrieves the port and host from the configuration and listens for incoming connections.
  * @param {import('fastify').FastifyInstance} app The Fastify application to start.
@@ -9,45 +47,18 @@ import { getConfig } from '../config/config.js';
  * @returns {Promise} Resolves when the server has started successfully.
  */
 export function startServer(app, server) { 
-  
-  // Add server parameter
   if (app.server) {
     throw new Error('Server is already running');
   }
 
-  app.server = server;  // Assign server to app.server here
+  app.server = server;
 
   const config = getConfig();
   const { port, host } = config.server;
 
   // Handle system signals
-  process.on('SIGINT', () => {
-    if (app.server) {
-      app.server.close((err) => {  // Add err parameter
-        if (err) {
-          app.log.error('Server close failed');  // Log error message
-        } else {
-          app.log.info('Server closed due to SIGINT signal');
-        }
-      });
-    }
-  });
-
-  let isClosed = false;
-
-  process.on('SIGTERM', () => {
-    if (!app.server) return;
-  
-    app.log.info('Received SIGTERM signal, shutting down server...');
-  
-    app.server.close(() => {
-      if (isClosed) return;
-  
-      isClosed = true;
-      app.log.info('Server closed successfully');  
-      process.exit(0);
-    });
-  });
+  process.on('SIGINT', () => handleSIGINT(app));
+  process.on('SIGTERM', () => handleSIGTERM(app));
 
   return new Promise((resolve, reject) => {
     app.listen(port, host, (err, server) => {
@@ -65,4 +76,3 @@ export function startServer(app, server) {
     });
   });
 }
-
